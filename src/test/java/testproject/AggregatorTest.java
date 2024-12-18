5eee2014ac0ae881f691fa6a939f700adc5a87f5
yokomin12 <alinamalinagames@gmail.com>
2024-12-18 21:45:36 +0300

    jacoco

--- src/test/java/testproject/AggregatorTest.java
+++ src/test/java/testproject/AggregatorTest.java
@@ -27,4 +27,14 @@ public class AggregatorTest {
         agr.addValue(val);
         assertThat(agr.getSum()).isEqualTo(val * 2);
     }
+
+    @Test
+    public void testResetting(){
+        Aggregator agr = new Aggregator();
+        double val = 15;
+        agr.addValue(val);
+        agr.addValue(val);
+        agr.reset();
+        assertThat(agr.getSum()).isEqualTo(0);
+    }
 }
